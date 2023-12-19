5df18526836368216f83cf16fb880adec2f770d3
Najeh <benrebehn@gmail.com>
2023-12-19 17:49:01 +0100

    first commit

---
+++ src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFormGroup: FormGroup = new FormGroup({}); // You can initialize it with an empty form group
+
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }
+      )
+      
+    })
+
+}
+onSubmit() {
+  console.log("Handling the submit button");
+  console.log(this.checkoutFormGroup.get('customer')!.value);
+  console.log("The email address is " + this.checkoutFormGroup.get('customer')!.value.email);
+}
+}
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+  addToCart(theCartItem: CartItem) {
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem | undefined = undefined;
+
+    if (this.cartItems.length > 0) {
+      existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id);
+      alreadyExistsInCart = !!existingCartItem;
+    }
+
+    if (alreadyExistsInCart) {
+      if (existingCartItem && existingCartItem.quantity !== undefined) {
+        existingCartItem.quantity++;
+      }
+    } else {
+      this.cartItems.push(theCartItem);
+    }
+
+    this.computeCartTotals();
+  }
+
+  computeCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for (let currentCartItem of this.cartItems) {
+      if (currentCartItem.quantity !== undefined && currentCartItem.unitPrice !== undefined) {
+        totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+        totalQuantityValue += currentCartItem.quantity;
+      }
+    }
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    this.logCartData(totalPriceValue, totalQuantityValue);
+  }
+
+  logCartData(totalPriceValue: number, totalQuantityValue: number) {
+    console.log('Contents of the cart');
+    for (let tempCartItem of this.cartItems) {
+      const quantity = tempCartItem.quantity ?? 0;
+      const unitPrice = tempCartItem.unitPrice ?? 0;
+      const subTotalPrice = quantity * unitPrice;
+      console.log(`name: ${tempCartItem.name}, quantity=${quantity}, unitPrice=${unitPrice}, subTotalPrice=${subTotalPrice}`);
+    }
+
+    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
+    console.log('----');
+  }
+  decrementQuantity(theCartItem: CartItem) {
+
+
+    if (theCartItem.quantity !== undefined) {
+      theCartItem.quantity--;
+      if (theCartItem.quantity === 0) {
+
+
+      this.remove(theCartItem);
+    }
+    else {
+      this.computeCartTotals();
+    }
+  }
+  }
+  remove(theCartItem: CartItem) {
+
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === theCartItem.id );
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
+  }
+}
